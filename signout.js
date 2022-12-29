391c8d6a452472790a30665115f1345751ab8a40
biddet <96024863+biddet@users.noreply.github.com>
2022-12-29 04:04:52 -0500

    Update signout.js

--- signout.js
+++ signout.js
@@ -1,9 +1,9 @@
-function signOut() {
-firebase.auth().signOut().then(function() {
-  // Sign-out was successful
-  console.log('User signed out');
-}).catch(function(error) {
-  // An error occurred
-  console.error(error);
-});
+function logout() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out was successful
+    console.log('User signed out');
+  }).catch(function(error) {
+    // An error occurred
+    console.error(error);
+  });
 }
